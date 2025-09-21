b66b121dbb2850cdfe089e5074a043309b95b1d6
Aleah P. <92504607+AleahPorras@users.noreply.github.com>
2025-09-21 11:32:05 -0700

    Updated files

--- genetic.py
+++ genetic.py
@@ -1,3 +1,54 @@
-import board
+from board import Board
+import random
+import time
 
-print(board.main())
+my_board = Board(5)
+
+###------------------------------------------------------------------------###
+## Generates the random populations to compare later on
+# Status: Done
+def population(size):
+
+    population = []
+    for _ in range(size):
+        my_board = Board(5)
+        chromosome = my_board.encode()
+        population.append(chromosome)
+    # return my_board.get_map()
+    return population
+    pass
+
+def fitness_scores(random_populations):
+    score = []
+    temp = Board(5)
+    for i in random_populations:
+        temp.decode(i)
+        fitness = temp.get_fitness()
+        score.append(fitness)
+
+    return score
+
+def selection():
+    pass
+
+def crossover():
+    pass
+
+def mutation():
+    pass
+
+
+def main():
+    # starting_timer = time.time()
+
+    # ending_timer = time.time()
+
+    # total_time = (ending_timer - starting_timer)*1000
+    # print(f"Running time: {total_time}ms")
+    my_board.print_map()
+
+    random_populations = population(5)
+    print(random_populations)
+    print(fitness_scores(random_populations))
+
+main()
